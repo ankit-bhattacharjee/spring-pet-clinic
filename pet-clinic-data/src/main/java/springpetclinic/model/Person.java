411f267fde3d6fcb7c479770b4fb7e7dafc3718f
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-14 14:58:55 +0530

    Id property of model class person moved to BaseEntity.

--- pet-clinic-data/src/main/java/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/springpetclinic/model/Person.java
@@ -1,18 +1,9 @@
 package springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
-    private Long id;
     private String firstName;
     private String lastName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
