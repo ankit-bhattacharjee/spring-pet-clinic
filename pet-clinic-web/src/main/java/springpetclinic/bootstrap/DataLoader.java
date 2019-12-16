267e157d8d7f148040c4568c616011208434def4
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-16 22:05:17 +0530

    Added List Of Owners on Page. Closes #15.

--- pet-clinic-web/src/main/java/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/springpetclinic/bootstrap/DataLoader.java
@@ -10,11 +10,11 @@ import springpetclinic.service.VetDbService;
 @Component
 public class DataLoader implements CommandLineRunner {
 
-    private final OwnerDbService ownerService;
+    private final OwnerDbService ownerDbService;
     private final VetDbService vetDbService;
 
-    public DataLoader(OwnerDbService ownerService, VetDbService vetDbService) {
-        this.ownerService = ownerService;
+    public DataLoader(OwnerDbService ownerDbService, VetDbService vetDbService) {
+        this.ownerDbService = ownerDbService;
         this.vetDbService = vetDbService;
     }
 
@@ -24,22 +24,25 @@ public class DataLoader implements CommandLineRunner {
         owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Myers");
+        ownerDbService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setId(2L);
         owner2.setFirstName("Freddy");
         owner2.setLastName("Kruger");
+        ownerDbService.save(owner2);
 
         Vet vet1 = new Vet();
         vet1.setId(3L);
         vet1.setFirstName("Jason");
         vet1.setLastName("Vorhess");
+        vetDbService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setId(4L);
         vet2.setFirstName("Chucky");
         vet2.setLastName("Chuck");
-
+        vetDbService.save(vet2);
 
     }
 }
--- pet-clinic-web/src/main/java/springpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/springpetclinic/controller/OwnerController.java
@@ -1,14 +1,23 @@
 package springpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springpetclinic.service.OwnerDbService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerDbService ownerDbService;
+
+    public OwnerController(OwnerDbService ownerDbService) {
+        this.ownerDbService = ownerDbService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("owners", ownerDbService.findAll());
         return "owners/index";
     }
 }
