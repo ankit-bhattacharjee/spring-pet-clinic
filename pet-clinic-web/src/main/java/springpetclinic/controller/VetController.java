9874383b16dfc47987508826ae8cd84216d24604
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-16 22:09:33 +0530

    Added List Of Vets on Page. Closes #14.

--- pet-clinic-web/src/main/java/springpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/springpetclinic/controller/VetController.java
@@ -1,14 +1,23 @@
 package springpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springpetclinic.service.VetDbService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetDbService vetDbService;
+
+    public VetController(VetDbService vetDbService) {
+        this.vetDbService = vetDbService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("vets", vetDbService.findAll());
         return "vets/index";
     }
 }
