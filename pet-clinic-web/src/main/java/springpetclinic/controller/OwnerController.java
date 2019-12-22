32809abf4528d035d2ecf19ac02f8812a846573b
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-22 19:09:56 +0530

    Fixed broken links with template page. Part of issue #44.

--- pet-clinic-web/src/main/java/springpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/springpetclinic/controller/OwnerController.java
@@ -20,4 +20,9 @@ public class OwnerController {
         model.addAttribute("owners", ownerDbService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notImplemented";
+    }
 }
