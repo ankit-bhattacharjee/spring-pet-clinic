73a57833e45dbd7bddfa94d207cb4663aa51afcd
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-22 18:53:18 +0530

    Added Cascade inserts to vets specialties. Part of issue #33.

--- pet-clinic-data/src/main/java/springpetclinic/service/map/VetMapService.java
+++ pet-clinic-data/src/main/java/springpetclinic/service/map/VetMapService.java
@@ -1,10 +1,30 @@
 package springpetclinic.service.map;
 
 import org.springframework.stereotype.Service;
+import springpetclinic.model.Specialty;
 import springpetclinic.model.Vet;
+import springpetclinic.service.SpecialtyService;
 import springpetclinic.service.VetDbService;
 
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetDbService {
 
+    private final SpecialtyService specialtyService;
+
+    public VetMapService(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        if (object.getSpecialities().size() > 0) {
+            object.getSpecialities().forEach(specialty -> {
+                if (specialty.getId() == null) {
+                    Specialty savedSpecialty = specialtyService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
+        return super.save(object);
+    }
 }
