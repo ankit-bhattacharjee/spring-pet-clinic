29ad1167cfe96ffee5acc015af99d4b062913654
ankit-bhattacharjee <ankit-bhattacharjee@github.com>
2019-12-24 14:54:06 +0530

    Implemented Owner Entity. Closes #35.

--- pet-clinic-data/src/main/java/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/springpetclinic/model/PetType.java
@@ -1,6 +1,13 @@
 package springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Types")
 public class PetType extends BaseEntity {
+    @Column(name = "name")
     private String name;
 
     public String getName() {
