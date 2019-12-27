2f8800ed60c7131b1d099efc44ef30e35e3f64c2
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-27 17:01:49 +0530

    Added Project Lombok. Closes #52.

--- pet-clinic-data/src/main/java/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/springpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package springpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -12,11 +19,4 @@ public class Vet extends Person {
             , inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialities = new HashSet<>();
 
-    public Set<Specialty> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Specialty> specialities) {
-        this.specialities = specialities;
-    }
 }
--- pet-clinic-data/src/main/java/springpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/springpetclinic/model/Visit.java
@@ -1,8 +1,15 @@
 package springpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
@@ -14,27 +21,4 @@ public class Visit extends BaseEntity {
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
-    public LocalDate getLocalDate() {
-        return localDate;
-    }
-
-    public void setLocalDate(LocalDate localDate) {
-        this.localDate = localDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
