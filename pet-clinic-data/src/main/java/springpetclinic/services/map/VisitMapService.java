3043469680c2b5575ff9fb4e0a2f1783ef7d503b
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-27 16:05:19 +0530

    Added VetMapService Implementation. Closes #47.

--- pet-clinic-data/src/main/java/springpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/springpetclinic/services/map/VisitMapService.java
@@ -3,10 +3,17 @@ package springpetclinic.services.map;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import springpetclinic.model.Visit;
-import springpetclinic.services.CrudService;
 import springpetclinic.services.VisitService;
 
 @Service
 @Profile("map")
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null)
+            throw new RuntimeException("Invalid Visit");
+        return super.save(object);
+    }
 }
