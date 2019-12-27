0db9790dc8e7bec46780a61a4e1570ba8e35d9b9
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-27 20:00:21 +0530

    Migrated to JUnit 5. Closes #4.

---
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class SpringPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
