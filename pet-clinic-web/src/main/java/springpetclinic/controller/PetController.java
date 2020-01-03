ff6b31cedd48e1faf153a59e168ff34e52f101fb
ankit-bhattacharjee <ankit-bhattacharjee@github.com>
2020-01-03 15:38:06 +0530

    Added model attributes for pet , closes #57.

---
+++ pet-clinic-web/src/main/java/springpetclinic/controller/PetController.java
@@ -0,0 +1,45 @@
+package springpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springpetclinic.model.Owner;
+import springpetclinic.model.PetType;
+import springpetclinic.services.OwnerService;
+import springpetclinic.services.PetService;
+import springpetclinic.services.PetTypeService;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
+
+    public PetController(PetService petService, PetTypeService petTypeService, OwnerService ownerService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+        this.ownerService = ownerService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initBinderOwner(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
